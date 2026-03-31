5b6a04977c47d4d43be309fdf963076c7944d42c
umarxdd <mihr.e.kahyan@gmail.com>
2026-04-01 01:12:14 +0500

    fix gsap trial to gsap

--- src/components/utils/initialFX.ts
+++ src/components/utils/initialFX.ts
@@ -1,5 +1,5 @@
 // import { SplitText } from "gsap-trial/SplitText";
-import SplitText from "gsap-trial/SplitText";
+import SplitText from "gsap/SplitText";
 import gsap from "gsap";
 import { smoother } from "../Navbar";
 
